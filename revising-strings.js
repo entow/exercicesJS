97346fd80610c91f47b730071baead237f1a2db1
Antonin Reillon <antonin.reillon@ynov.com>
2018-02-14 11:52:29 +0100

    Les 8 premiers exercices

---
+++ revising-strings.js
@@ -0,0 +1,3 @@
+var pizza = 'pizza is alright';
+.replace(wonderful);
+console.log(.replace);
---
+++ rounding-numbers.js
@@ -0,0 +1,5 @@
+var roundUp = '1.5';
+Math.round();
+
+var rounded = Math.round(roundUp);
+console.log(rounded);
